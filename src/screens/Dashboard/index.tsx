7ab1023aca5e1562829a93dab2543f6aa0fb6aa2
mardsonferreira <mardsonferreira25@gmail.com>
2022-03-09 08:06:18 -0300

    Adding header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React from 'react';
 
-import { Container, Title } from "./styles";
+import {
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+} from './styles';
 
 export function Dashboard() {
-  return (
-    <Container>
-      <Title>Dashboard</Title>
-    </Container>
-  );
+    return (
+        <Container>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo
+                            source={{
+                                uri: 'https://avatars.githubusercontent.com/u/5270702?v=4',
+                            }}
+                        />
+
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>Mardson</UserName>
+                        </User>
+                    </UserInfo>
+                </UserWrapper>
+            </Header>
+        </Container>
+    );
 }
