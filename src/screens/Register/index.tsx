52bcdd643571afaaaf6c9cc9159a9a40febbc259
mardsonferreira <mardsonferreira25@gmail.com>
2022-03-21 09:59:24 -0300

    Creating button component

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
 import { Input } from '../../components/Form/Input';
+import { Button } from '../../components/Form/Button';
 
-import { Container, Header, Title, Form } from './styles';
+import { Container, Header, Title, Form, Fields } from './styles';
 
 export function Register() {
     return (
@@ -12,9 +13,13 @@ export function Register() {
             </Header>
 
             <Form>
-                <Input placeholder='Nome' />
+                <Fields>
+                    <Input placeholder="Nome" />
 
-                <Input placeholder='Preço' />
+                    <Input placeholder="Preço" />
+                </Fields>
+
+                <Button title="Enviar" />
             </Form>
         </Container>
     );
