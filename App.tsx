ab0d566195ae2e6b28a46cacea769da3bca2be3f
mardsonferreira <mardsonferreira25@gmail.com>
2022-03-29 21:01:02 -0300

    Adding tab navigation

--- App.tsx
+++ App.tsx
@@ -10,9 +10,9 @@ import {
 } from '@expo-google-fonts/poppins';
 
 import theme from './src/global/styles/theme';
+import { NavigationContainer } from '@react-navigation/native';
 
-// import { Dashboard } from './src/screens/Dashboard';
-import { Register } from './src/screens/Register';
+import { AppRoutes } from './src/routes/app.routes';
 
 export default function App() {
     const [fonstLoaded] = useFonts({
@@ -27,7 +27,9 @@ export default function App() {
 
     return (
         <ThemeProvider theme={theme}>
-            <Register />
+            <NavigationContainer>
+                <AppRoutes />
+            </NavigationContainer>
         </ThemeProvider>
     );
 }
