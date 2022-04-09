6d40618a064b8972cbe3a7f89f3883b877dc8e5e
mardsonferreira <mardsonferreira25@gmail.com>
2022-04-08 21:06:40 -0300

    Rendering graphics

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { VictoryPie } from 'victory-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import { useTheme } from 'styled-components';
 
 import { HistoryCard } from '../../components/HistoryCard';
 import { categories } from '../../utils/categories';
 
-import { Container, Header, Title, Content } from './styles';
+import { Container, Header, Title, Content, ChartContainer } from './styles';
 interface TransactionData {
     type: 'income' | 'expense';
     name: string;
@@ -16,12 +20,15 @@ interface TransactionData {
 interface CategoryData {
     key: string;
     name: string;
-    total: string;
+    total: number;
+    totalFormatted: string;
     color: string;
+    percent: string;
 }
 
 export function Resume() {
     const [categoriesList, setCategoriesList] = useState<CategoryData[]>([]);
+    const theme = useTheme();
 
     async function loadData() {
         const dataKey = '@gofinances:transactions';
@@ -34,6 +41,13 @@ export function Resume() {
             (expense: TransactionData) => expense.type == 'expense'
         );
 
+        const expensesTotal = expenses.reduce(
+            (acc: number, expense: TransactionData) => {
+                return acc + Number(expense.amount);
+            },
+            0
+        );
+
         categories.forEach((category) => {
             let categorySum = 0;
             expenses.forEach((expense) => {
@@ -43,15 +57,23 @@ export function Resume() {
             });
 
             if (categorySum > 0) {
-                const total = categorySum.toLocaleString('pt-BR', {
+                const totalFormatted = categorySum.toLocaleString('pt-BR', {
                     style: 'currency',
                     currency: 'BRL',
                 });
+
+                const percent = `${(
+                    (categorySum / expensesTotal) *
+                    100
+                ).toFixed(0)}%`;
+
                 totalByCategory.push({
-                    key: category.key, 
+                    key: category.key,
                     name: category.name,
                     color: category.color,
-                    total,
+                    total: categorySum,
+                    totalFormatted,
+                    percent,
                 });
             }
         });
@@ -69,11 +91,29 @@ export function Resume() {
             </Header>
 
             <Content>
+                <ChartContainer>
+                    <VictoryPie
+                        data={categoriesList}
+                        colorScale={categoriesList.map(
+                            (category) => category.color
+                        )}
+                        style={{
+                            labels: {
+                                fontSize: RFValue(18),
+                                fontWeight: 'bold',
+                                fill: theme.colors.shape,
+                            },
+                        }}
+                        labelRadius={50}
+                        x="percent"
+                        y="total"
+                    />
+                </ChartContainer>
                 {categoriesList.map((item) => (
                     <HistoryCard
                         key={item.key}
                         title={item.name}
-                        amount={item.total}
+                        amount={item.totalFormatted}
                         color={item.color}
                     />
                 ))}
