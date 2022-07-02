b1cc176dd54444aa01ebf0983a62291af30a77f1
mardsonferreira <mardsonferreira25@gmail.com>
2022-07-01 23:19:06 -0300

    Custom mocks

--- src/hooks/auth.spec.tsx
+++ src/hooks/auth.spec.tsx
@@ -1,5 +1,8 @@
 import { renderHook, act } from '@testing-library/react-hooks';
+import { mocked } from 'jest-mock';
 import fetchMock from 'jest-fetch-mock';
+import { startAsync } from 'expo-auth-session';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { AuthProvider, useAuth } from './auth';
 
@@ -12,19 +15,23 @@ const userTest = {
     photo: 'any_photo.png',
 };
 
-jest.mock('expo-auth-session', () => {
-    return {
-        startAsync: () => ({
+jest.mock('expo-auth-session');
+
+describe('Auth Hook', () => {
+    beforeEach(async () => {
+        const userCollectionKey = '@gofinances:users';
+        await AsyncStorage.removeItem(userCollectionKey);
+    });
+
+    it('should be able to sign in with Google account existing', async () => {
+        const googleMocked = mocked(startAsync as any);
+        googleMocked.mockReturnValue({
             type: 'success',
             params: {
                 access_token: 'any_token',
             },
-        }),
-    };
-});
+        });
 
-describe('Auth Hook', () => {
-    it('should be able to sign in with Google account existing', async () => {
         fetchMock.mockResponseOnce(JSON.stringify(userTest));
 
         const { result } = renderHook(() => useAuth(), {
@@ -34,5 +41,20 @@ describe('Auth Hook', () => {
         await act(() => result.current.signInWithGoogle());
 
         expect(result.current.user.email).toBe(userTest.email);
+    });
+
+    it('user should not connect if cancel authentication with google', async () => {
+        const googleMocked = mocked(startAsync as any);
+        googleMocked.mockReturnValue({
+            type: 'cancel',
+        });
+
+        const { result } = renderHook(() => useAuth(), {
+            wrapper: AuthProvider,
+        });
+
+        await act(() => result.current.signInWithGoogle());
+
+        expect(result.current.user).not.toHaveProperty('id');
     });
 });
